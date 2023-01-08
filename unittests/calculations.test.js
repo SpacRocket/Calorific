49853371453edfceaafe5b867d7fd9bdc065a413
SpacRocket <95596717+SpacRocket@users.noreply.github.com>
2023-01-08 23:06:21 +0100

    better unittest

--- unittests/calculations.test.js
+++ unittests/calculations.test.js
@@ -2,5 +2,5 @@ const recipeNutritients = require('../nutrients.js');
 
 test('Create a request', async () => {
     const result = await recipeNutritients("10 medium sized eggs and 100g of butter. It's a perfect recipe!");
-    console.log(result);
+    return isNaN(result.kcal) && result.kcal != 0;
 });
